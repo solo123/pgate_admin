df6859a829d2ace2c15ae237f6f1f23d5f9935c2
jimmy <solo123@21cn.com>
2016-10-11 14:02:36 +0800

    data

--- db/migrate/20161010083640_create_post_dat.rb
+++ db/migrate/20161010083640_create_post_dat.rb
@@ -1,13 +1,13 @@
-class CreatePostData < ActiveRecord::Migration[5.0]
+class CreatePostDat < ActiveRecord::Migration[5.0]
   def change
-    create_table :post_data do |t|
+    create_table :post_dats do |t|
       t.references :sender, polymorphic: true, index: true
       t.string :url
       t.string :data
       t.string :response
       t.string :body
       t.text :error_message
-      
+
       t.timestamps
     end
   end
