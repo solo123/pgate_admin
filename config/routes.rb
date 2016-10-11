c93e22f562934bec5af7b50a296decfcb671a392
jimmy <solo123@21cn.com>
2016-10-11 12:27:14 +0800

    test

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,11 @@ Rails.application.routes.draw do
     end
   end
   namespace :test_pages do
-    %w(do_post gen_qrcode pay pay_t1 pay_app_t0 pay_app_t1 query_openid).each do |action|
+    %w(gen_qrcode pay pay_t1 pay_app_t0 pay_app_t1 query_openid).each do |action|
       get action, action: action
     end
   end
+  post 'test_pages', to: 'test_pages#do_post'
 
   post 'recv_sm', to: 'recv_sm#recv_post'
   resources :kaifu_gateways
