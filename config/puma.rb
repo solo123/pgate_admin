b89af3e3bef05b5e43bb0e4eab7c45dfeaed1ac1
jimmy <solo123@21cn.com>
2016-10-15 03:04:49 +0800

    mina

--- config/puma.rb
+++ config/puma.rb
@@ -1,17 +1,22 @@
 #!/usr/bin/env puma
 
 # start puma with:
-# bundle exec puma -e production -C /home/rb/work/pgate_admin/config/puma.rb
+#bundle exec puma -e production -C config/puma.rb
+#bundle exec pumactl --state tmp/sockets/puma.state stop
+#bundle exec pumactl --state tmp/sockets/puma.state restart
 
-application_path = '/home/rb/work/pgate_admin'
 railsenv = 'production'
+application_path = '/home/rb/work/pgate_admin'
+threads 2, 32 # minimum 2 threads, maximum 64 threads
+workers 2
+
 directory application_path
 environment railsenv
-daemonize true
 pidfile "#{application_path}/tmp/pids/puma-#{railsenv}.pid"
 state_path "#{application_path}/tmp/pids/puma-#{railsenv}.state"
-stdout_redirect
-"#{application_path}/log/puma-#{railsenv}.stdout.log"
-"#{application_path}/log/puma-#{railsenv}.stderr.log"
-threads 0, 16
+stdout_redirect "#{application_path}/log/puma-#{railsenv}.stdout.log", "#{application_path}/log/puma-#{railsenv}.stderr.log"
+
 bind "unix://#{application_path}/tmp/sockets/#{railsenv}.socket"
+worker_timeout 60
+daemonize true
+preload_app!
