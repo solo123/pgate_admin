eb7453df37c22c9a9226d2f2a17a7f84b35ccb21
jimmy <solo123@21cn.com>
2016-12-14 23:05:05 +0800

    agent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212160356) do
+ActiveRecord::Schema.define(version: 20161213143422) do
+
+  create_table "agents", force: :cascade do |t|
+    t.integer  "org_id"
+    t.string   "roles"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["confirmation_token"], name: "index_agents_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_agents_on_email", unique: true
+    t.index ["org_id"], name: "index_agents_on_org_id"
+    t.index ["reset_password_token"], name: "index_agents_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_agents_on_unlock_token", unique: true
+  end
 
   create_table "app_configs", force: :cascade do |t|
     t.string   "group"
