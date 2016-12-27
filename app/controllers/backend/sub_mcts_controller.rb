4536bb0af2e6af62a22d9eb3c2bdfbf08a8d237b
dalezhang <17652709@qq.com>
2016-12-27 15:57:18 +0800

    hz进件

--- app/controllers/backend/sub_mcts_controller.rb
+++ app/controllers/backend/sub_mcts_controller.rb
@@ -1,2 +1,31 @@
 class Backend::SubMctsController < ResourcesController
+	def update
+		load_object
+		params.permit!
+		@object.attributes = params[object_name.singularize.parameterize('_')]
+		if @object.changed_for_autosave?
+			#@changes = @object.all_changes
+			if @object.save
+			else
+				flash[:error] = @object.errors.full_messages.to_sentence
+				@no_log = 1
+			end
+		end
+    case params[:commit]
+    when '进件'
+      biz = Biz::HzIntfcApi.new(@object)
+      biz.prepare_request
+      if biz.err_code == '00'
+        biz.send_zx_intfc
+        flash[:error] = biz.err_desc unless biz.err_code == '00'
+      else
+        flash[:error] = biz.err_desc
+      end
+    when '查询'
+      biz = Biz::HzIntfcApi.new(@object)
+      biz.send_zx_query
+      flash[:error] = biz.err_desc unless biz.err_code == '00'
+    end
+    redirect_to edit_org_path(@object.org)
+	end
 end
---
+++ app/models/biz/hz_intfc_api.rb
@@ -0,0 +1,95 @@
+module Biz
+  class HzIntfcApi < BizBase
+    attr_accessor :sub_mct, :xml
+
+    def initialize(sub_mct)
+      @sub_mct = sub_mct
+    end
+
+    def prepare_request
+      builder = Nokogiri::XML::Builder.new(:encoding => 'GBK') do |xml|
+        xml.AIPG {
+          xml.INFO {
+            xml.TRX_CODE '100012'
+            xml.VERSION '01'
+            xml.REQ_SN 'ORD-' + Time.now.to_i.to_s + '-001'
+            xml.SIGNED_MSG '[sign]'
+          }
+          xml.BODY {
+            xml.TRANS_DETAIL {
+              xml.IN_CHANNEL '1'
+              xml.QRCODE_CHANNEL '1'
+              xml.MERCHANT_ID @sub_mct.parent_mch_id # 合众主商户代码(必填)
+              xml.SUBMERCHANT_CODE @sub_mct.mch_id
+              xml.SUBMERCHANT_NAME @sub_mct.org.merchant.full_name
+              xml.SUBMERCHANT_SHORTNAME @sub_mct.org.name
+              xml.SUBMERCHANT_ADDRESS @sub_mct.org.merchant.address
+              xml.SUBMERCHANT_SERVICEPHONE @sub_mct.org.merchant.service_tel
+              xml.SUBMERCHANT_CATEGORY @sub_mct.business_type
+            }
+          }
+        }
+      end
+      @xml = builder.to_xml
+    end
+
+    def sign(mabs)
+      sign = ''
+      begin
+        socket = TCPSocket.open('127.0.0.1', 9001)
+        socket.write("HzSign\0")
+        socket.write(data)
+        socket.write("\0")
+        sign = socket.read
+      rescue => e
+        puts "Error:", e.message
+      end
+      sign
+    end
+
+    def send_zx_intfc
+      xml_str = @xml.gsub('[sign]', '')
+      xml_utf = xml_str.encode('utf-8', 'gbk')
+      sn = sign(xml_utf)
+      xml_sn = xml_str.gsub('<SIGNED_MSG></SIGNED_MSG>', "<SIGNED_MSG>#{sn}</SIGNED_MSG>")
+      gzip = ActiveSupport::Gzip.compress(xml_sn)
+      b64 = Base64.encode64(gzip)
+      begin
+        resp = HTTParty.post(url, body: b64, headers: {"Content-Type": "text/plain; charset=ISO-8859-1"})
+        if resp.body.present?
+          txt_gzip = Base64.decode64(resp.body)
+          txt = ActiveSupport::Gzip.decompress(txt_gzip)
+          txt.force_encoding('gbk')
+          txt_no = txt.gsub(/<SIGNED_MSG>(.|\n)*<\/SIGNED_MSG>/, '<SIGNED_MSG></SIGNED_MSG>')
+          rr = txt.match(/<SIGNED_MSG>((.|\n)*)<\/SIGNED_MSG>/)
+          return_key = rr[1]
+          txt_no_utf = txt_no.encode('utf-8', 'gbk')
+          unless verify(txt_no_utf, return_key)
+            @err_code = '20'
+            @err_desc = '验签错误'
+          end
+          xml = Nokogiri::XML(txt_no_utf.resp_body)
+          if xml.xpath("//RET_CODE").text == '0000'
+            @sub_mch.status = 1
+            @sub_mch.save!
+          else
+            @err_code = '20'
+            @err_desc = xml.xpath("//ERR_MSG").text
+          end
+        else
+          @err_code = '20'
+          @err_desc = 'null返回值'
+        end
+      rescue => e
+        @err_code = '21'
+        @err_desc = '链接错误：' + e.message
+      end
+    end
+
+    def verify(data, p7_key)
+      pkcs7 = OpenSSL::PKCS7.new(Base64.decode64(p7_key))
+      pkcs7.verify(pkcs7.certificates, OpenSSL::X509::Store.new, data, OpenSSL::PKCS7::NOVERIFY)
+    end
+
+  end
+end
